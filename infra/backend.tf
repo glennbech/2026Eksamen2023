460ca2174843017318824ef35866c627b27d6142
SamuelEBG <samuel.ebgonzales@gmail.com>
2023-11-23 15:30:16 +0100

    major refactoring

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "${var.candidate}-state/apprunner-actions.state"
+    key    = "kandidat-2026-state/apprunner-actions.state"
     region = "eu-north-1"
   }
 }
