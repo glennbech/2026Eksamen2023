721627116e20dedacef8eb8a3ff165b52bb2427d
glennbech <glenn.bech@gmail.com>
2023-12-16 18:52:22 +0100

    testing testing

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "kandidat-2026-state/apprunner-actions.state"
+    key    = "kandidat-2026-state/apprunner-actions-sensur.state"
     region = "eu-north-1"
   }
 }
